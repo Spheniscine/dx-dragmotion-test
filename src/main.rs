467977459dd10a79940856f1f4f0bacc29452296
Spheniscine <spheniscine@gmail.com>
2026-05-08 22:15:34 +0800

    simple cards test

--- src/main.rs
+++ src/main.rs
@@ -27,15 +27,62 @@ pub fn Hero() -> Element {
     rsx! {
         div {
             id: "hero",
-            img { src: HEADER_SVG, id: "header", draggable: true }
-            div { id: "links",
-                a { href: "https://dioxuslabs.com/learn/0.7/", draggable: true, "📚 Learn Dioxus" }
-                a { href: "https://dioxuslabs.com/awesome", "🚀 Awesome Dioxus" }
-                a { href: "https://github.com/dioxus-community/", "📡 Community Libraries" }
-                a { href: "https://github.com/DioxusLabs/sdk", "⚙️ Dioxus Development Kit" }
-                a { href: "https://marketplace.visualstudio.com/items?itemName=DioxusLabs.dioxus", "💫 VSCode Extension" }
-                a { href: "https://discord.gg/XgGxMSkvUM", "👋 Community Discord" }
+            // img { src: HEADER_SVG, id: "header", draggable: true }
+            // div { id: "links",
+            //     a { href: "https://dioxuslabs.com/learn/0.7/", draggable: true, "📚 Learn Dioxus" }
+            //     a { href: "https://dioxuslabs.com/awesome", "🚀 Awesome Dioxus" }
+            //     a { href: "https://github.com/dioxus-community/", "📡 Community Libraries" }
+            //     a { href: "https://github.com/DioxusLabs/sdk", "⚙️ Dioxus Development Kit" }
+            //     a { href: "https://marketplace.visualstudio.com/items?itemName=DioxusLabs.dioxus", "💫 VSCode Extension" }
+            //     a { href: "https://discord.gg/XgGxMSkvUM", "👋 Community Discord" }
+            // }
+
+            div {
+                margin: "2rem",
+                display: "flex",
+                flex_direction: "row",
+                for i in 0..8 {
+                    if i > 0 {
+                        div {
+                            width: "1rem",
+                        }
+                    }
+                    div {
+                        style: "place-items: center",
+                        background_color: "#fff",
+                        width: "9.5rem",
+                        height: "12rem",
+                        border: "0.5rem solid #000",
+                        border_radius: "1.5rem",
+                        display: "grid",
+                        grid_template_columns: "auto auto",
+                        font_size: "4rem",
+                        text_align: "center",
+                        padding: "0.5rem",
+
+                        div {
+                            font_family: "KaTeX_Main",
+                            text_align: "center",
+                            "13"
+                        },
+                        div {
+                            font_family: "KaTeX_Main",
+                            text_align: "center",
+                            "♠"
+                        },
+                        div {
+                            font_family: "KaTeX_Main",
+                            "♠"
+                        },
+                        div {
+                            font_family: "KaTeX_Main",
+                            "13"
+                        },
+                    }
+                }
+                
             }
+            
         }
     }
 }
